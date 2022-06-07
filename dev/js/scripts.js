0cd5c5a2f3cc36d44a60da630f7318afd37f45d2
HopeZucco <hmz34@drexel.edu>
2022-06-06 21:39:44 -0400

    background added and plane fade out change

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,9 +6,8 @@ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { MotionPathHelper } from "gsap/MotionPathHelper";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 // import { TweenMax } from "gsap/gsap-core";
-// import { TweenLite } from "gsap/gsap-core";
+// import { TweenLite } from "gsap/TweenLite";
 // import { TweenMax } from "gsap/TweenMax";
-// import { TweenMax, TimelineLite, Power2, Elastic, CSSPlugin } from "gsap/TweenMax";
 // import { TweenMax } from "gsap/TweenMaxBase";
 
 //Gsap Register Plugins
@@ -25,35 +24,41 @@ gsap.set(".airplaneNew",{alpha:1,x:"-=126",transformOrigin:"50% 50%"});
 gsap.set(".logoC",{drawSVG:"0%"});
 gsap.set(".theLine",{x:"+=77",y:"+=61",scale:.5,alpha:0});
 gsap.set(".airplaneNew",{rotation:200});
+gsap.set(".clouds",{scale:0.75});
 
 
 // gsap.set()
 gsap.set(".logoA",{alpha:0});
 
+function cloudMovement(){
+    mainTL.from(".clouds",{duration:4, xPercent:-200});
+    mainTL.to(".clouds",{duration:4,scale:1.25},"-=3");
+    
+}
+
 function airplaneIntro(){
     // mainTL.from(".airplaneNew",{y:"+=200",duration:2,ease:"power4.out"});
     // mainTL.to(".airplaneNew",{duration:2,alpha:1},"-=2");
-    mainTL.to(".airplaneNew",{motionPath:{path:"#airplanepath",align:"#airplanepath",autoRotate:true,alignOrigin:[0.5,0.5]},duration:3,ease:"power2.out"});
+    mainTL.to(".airplaneNew", {motionPath:{path:"#airplanepath",align:"#airplanepath",autoRotate:true,alignOrigin:[0.5,0.5]},duration:4.5,ease:"power2.out"}, "-=4");
 }
 
-
-// function planeToLogo(){
-//     // write that
-//     mainTL.to(".airplaneNew",{duration:.5,scale:0.75,alpha:0});
-//     mainTL.to(".logoA",{alpha:1,duration:.5},"-=.25");
-    
-//     // mainTL.to(".logoC",{drawSVG:"100%",alpha:1,duration:1,ease:"power4.inOut"});
-
-// }
 
 function planeToLogo(){
-
-    var tl = gsap.timeline({defaults: {duration: 1}}),
-airplane = document.getElementById("airplane");
-
-tl.to(airplane, {morphSVG:"#A"}, "+=1")
+    // write that
+    mainTL.to(".airplaneNew",{duration:.5,scale:0.75,alpha:0});
+    mainTL.to(".logoA",{alpha:1,duration:.5},"-=.25");
+    
+    // mainTL.to(".logoC",{drawSVG:"100%",alpha:1,duration:1,ease:"power4.inOut"});
 
 }
+
+// function planeToLogo(){
+
+//     var tl = gsap.timeline({defaults: {duration: 1}}),
+//     airplane = document.getElementsByClassName(".airplaneNew");
+
+//     tl.to(airplane, {morphSVG:".logoA"});
+// }
 
 function typeReveal(){
     // write that
@@ -69,6 +74,7 @@ function typeReveal2(){
 }
 
 mainTL
+.add(cloudMovement())
 .add(airplaneIntro())
 .add(planeToLogo())
 .add(typeReveal())
